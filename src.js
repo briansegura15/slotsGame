d0417ef9ac167402cfe01142e9f75c5af91edef3
Brian Segura <briansegura093@gmail.com>
2025-02-14 13:25:27 -0500

    happy vday babay

--- src.js
+++ src.js
@@ -4,18 +4,22 @@ const icon_width = 79,
   time_per_icon = 100,
   indexes = [0, 0, 0];
 
-git const roll = (reel, offset = 0) => {
+const roll = (reel, offset = 0) => {
   const delta =
     (offset + 2) * num_icons + Math.floor(Math.random() * num_icons);
   const style = getComputedStyle(reel),
     backgroundPositionY = parseFloat(style["background-position-y"]);
 
-  reel.style.transition = `background-position-y ${
-    8 + delta * time_per_icon
-  }ms`;
-  reel.style.backgroundPositionY = `${
-    backgroundPositionY + delta * icon_height
-  }px`;
+  return new Promise((resolve, reject) => {
+    reel.style.transition = `background-position-y ${
+      8 + delta * time_per_icon
+    }ms`;
+    reel.style.backgroundPositionY = `${
+      backgroundPositionY + delta * icon_height
+    }px`;
+
+    setTimeout(() => {}, 8 + delta * time_per_icon);
+  });
 };
 
 function rollAll() {
