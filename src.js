630b76ed1d120af30aa4ecbf3b67d350250455a2
Brian Segura <briansegura093@gmail.com>
2025-02-17 22:54:36 -0500

    js complete, css optimization needed

--- src.js
+++ src.js
@@ -20,16 +20,19 @@ const roll = (reel, offset = 0) => {
     (offset + 2) * num_icons + Math.floor(Math.random() * num_icons);
   const style = getComputedStyle(reel),
     backgroundPositionY = parseFloat(style["background-position-y"]);
+  (targetBackgroundPositionY = backgroundPositionY + delta * icon_height),
+    (normTargetBackgroundPositionY =
+      targetBackgroundPositionY % (num_icons * icon_height));
 
   return new Promise((resolve, reject) => {
     reel.style.transition = `background-position-y ${
       8 + delta * time_per_icon
     }ms`;
-    reel.style.backgroundPositionY = `${
-      backgroundPositionY + delta * icon_height
-    }px`;
+    reel.style.backgroundPositionY = `${targetBackgroundPositionY}px`;
 
     setTimeout(() => {
+      reel.style.transition = "none";
+      reel.style.backgroundPositionY = `${normTargetBackgroundPositionY}`;
       resolve(delta % num_icons);
     }, 8 + delta * time_per_icon);
   });
@@ -41,10 +44,13 @@ function rollAll() {
     deltas.forEach(
       (delta, i) => (indexes[i] = (indexes[i] + delta) % num_icons)
     );
-    console.log(indexes);
+    indexes.map(index => console.log(iconMap[index]));
 
     // check win conditions
-    setTimeout(rollAll, 10000);
+    if (indexes[0] == indexes[1] || (indexes[0] == indexes[1]) == indexes[2]) {
+      console.log("⭐ -----BIGGGG WINNNN------ ⭐");
+    }
+    setTimeout(rollAll, 2000);
   });
   // [...reelsList].map((reel, i) => {
   //   console.log(reel, i);
